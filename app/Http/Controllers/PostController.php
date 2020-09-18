f964496bb6fb0052b716f174b102c1fc64789933
henry <liboming88@yahoo.com>
2020-09-19 00:05:40 +0800

    add:custom paginator

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,6 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
+//        $articles = Article::with('author', 'category')->public()->orderBy('id', 'desc')->seoPaginate($pageSize, $this->listColumns, 'page', $page);
         return view('post.index');
     }
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Utils\CustomPaginator;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,8 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::directive('datetime', function($expression) {
+        Blade::directive('datetime', function ($expression) {
             return "<?php echo ($expression)->format('m/d/Y H:i'); ?>";
         });
+        // 为查询构建器注入自己实现的分页器方法
+        CustomPaginator::injectIntoBuilder();
     }
 }
