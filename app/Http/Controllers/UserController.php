6a748cd019d6e614a28c7ad9b245067b2a070b64
henry <liboming88@yahoo.com>
2020-09-21 09:30:59 +0800

    add:encrypt

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Log;
 use Illuminate\View\View;
 
@@ -21,5 +24,21 @@ class UserController extends Controller
         Log::info('Showing user profile for user: '.$id);
 
         return view('user.profile', ['user' => $user]);
+    }
+
+    /**
+     * Store a secret message for the user.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function storeSecret(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $user->fill([
+            'secret' => Crypt::encryptString($request->secret),
+        ])->save();
     }
 }
